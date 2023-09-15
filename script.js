027b94e6bd55687df7607a984b8a39b2bfa621bb
KomalRajS <komalrajs2002@gmail.com>
2023-09-15 13:58:25 +0530

    commit landing page

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeBg(bg,title) {
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelectorAll('.content');
+    banner.style.background = `url("../images/${bg}")`;
+    banner.style.backgroundSize = '100% 100%';
+    banner.style.backgroundRepeat = 'no-repeat';
+    banner.style.backgroundPosition = 'center';
+
+    contents.forEach(content => {
+        content.classList.remove('active');
+        if(content.classList.contains(title)) {
+            content.classList.add('active');
+        }
+    });
+}
